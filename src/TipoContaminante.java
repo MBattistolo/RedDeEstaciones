6ff43d807d49d23615940626b6606429b3ba46d4
Susana Marin B <randommuffin.0@gmail.com>
2026-08-13 09:20:43 -0500

    Create TipoContaminante.java

---
+++ src/TipoContaminante.java
@@ -0,0 +1,47 @@
+public class TipoContaminante {
+
+private String codigo;
+private String nombreContaminante;
+private double valorMinimo;
+private double valorMaximo;
+
+public TipoContaminante(String codigo, String nombreContaminante,
+double valorMinimo, double valorMaximo) {
+this.codigo = codigo;
+this.nombreContaminante = nombreContaminante;
+this.valorMinimo = valorMinimo;
+this.valorMaximo = valorMaximo;
+}
+
+public String getCodigo() {
+return codigo;
+}
+
+public String getNombreContaminante() {
+return nombreContaminante;
+}
+
+public double getValorMinimo() {
+return valorMinimo;
+}
+
+public double getValorMaximo() {
+return valorMaximo;
+}
+
+public void setCodigo(String codigo) {
+this.codigo = codigo;
+}
+
+public void setNombreContaminante(String nombreContaminante) {
+this.nombreContaminante = nombreContaminante;
+}
+
+public void setValorMinimo(double valorMinimo) {
+this.valorMinimo = valorMinimo;
+}
+
+public void setValorMaximo(double valorMaximo) {
+this.valorMaximo = valorMaximo;
+}
+}
